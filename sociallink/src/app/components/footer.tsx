57551fce8ce98dbb50da2996301046067be5c55e
leeuwz <jasperbos1234@gmail.com>
2024-09-10 23:15:02 +0200

    add footer

---
+++ sociallink/src/app/components/footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <>
+            <div className="flex justify-center py-4 font-bold text-white/80">
+                <p className="bg-gradient-to-r from-zinc-200/60 via-zinc-200 to-zinc-200/60 bg-clip-text text-transparent">
+                    @leeuwz <span className="text-white">🐈</span> &copy; 2024
+                </p>
+            </div>
+        </>
+    );
+}
