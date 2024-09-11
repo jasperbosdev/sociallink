7a71b2fe95573222a07f03ac5f4130f44f0a9a2e
Lionz <Jasperbos1234@gmail.com>
2024-09-11 22:31:38 +1200

    add particles to image container and other additions

--- sociallink/src/app/page.tsx
+++ sociallink/src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Tilt } from 'react-next-tilt';
+import ParticleEffectComponent from './components/utils/particles';
 
 export default function Home() {
   return (
@@ -16,19 +17,21 @@ export default function Home() {
         </div>
 
         <div className="relative flex justify-center w-2/3">
-
           <svg height="90%" width="60%" className="absolute left-0 top-[50%] z-10 translate-y-[-50%] rotate-6 text-indigo-600/50 opacity-100 blur-[60px]
           animate-fade animate-once animate-duration-[2800ms] animate-delay-[600ms]">
             <ellipse cx="50%" cy="50%" ry="50%" rx="50%" fill="currentColor"></ellipse>
           </svg>
-          
+
+          <div className="absolute top-0 left-0 w-full h-full z-15 pointer-events-none">
+            <ParticleEffectComponent className="w-full h-full animate-fade animate-once animate-duration-[2800ms] animate-delay-[400ms]" />
+          </div>
+
           <svg height="90%" width="70%" className="absolute right-0 top-[50%] z-10 translate-y-[-50%] text-[hsla(0_80%_51%_/_20%)] opacity-100 blur-[60px]
           animate-fade animate-once animate-duration-[2800ms] animate-delay-[600ms]">
             <ellipse cx="50%" cy="50%" ry="50%" rx="50%" fill="currentColor"></ellipse>
           </svg>
 
-          {/* Image with Tilt effect */}
-          <Tilt className="w-2/6 z-20" lineGlareEnable={false} tiltReverse={true} spotGlareEnable={false}>
+          <Tilt className="w-2/6 z-20" lineGlareEnable={false} tiltReverse={false} spotGlareEnable={false}>
             <img src="./static/images/placeholderc.png" className="w-full animate-fade animate-once animate-duration-[2800ms] animate-delay-[600ms]" />
           </Tilt>
         </div>
@@ -39,7 +42,7 @@ export default function Home() {
           </p>
           <div className="flex justify-center space-x-10 mt-5">
             <a href="/#">
-              <div className="flex items-center bg-neutral-950 py-2 px-5 rounded-xl transition duration-300">
+              <div className="flex items-center bg-neutral-950 py-2 px-5 rounded-xl transition duration-300 hover:shadow-md hover:shadow-white/60">
                 <i className="fas fa-tags mr-2 text-white fa-lg"></i>
                 <p className="text-white font-bold text-sm uppercase">Store</p>
               </div>
@@ -57,7 +60,7 @@ export default function Home() {
           <p className="font-bold text-white/60">
             Learn More
           </p>
-          <div className="animate-bounce">
+          <div className="animate-bounce animate-infinite">
             <i className='fas fa-arrow-down fa-2xl text-white/60'></i>
           </div>
         </div>
