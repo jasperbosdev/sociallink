807d3f6f29651a7058a9d43e5820551480e6b8be
Lionz <Jasperbos1234@gmail.com>
2024-09-12 00:25:08 +1200

    add border around button on hover (not final)

--- sociallink/src/app/page.tsx
+++ sociallink/src/app/page.tsx
@@ -42,10 +42,12 @@ export default function Home() {
           </p>
           <div className="flex justify-center space-x-10 mt-5">
             <a href="/#">
-              <div className="flex items-center bg-neutral-950 py-2 px-5 rounded-xl transition duration-300 hover:shadow-md hover:shadow-white/60">
+            <div className="relative bg-neutral-950 rounded-xl p-0.5 transition-all duration-[300ms] ease-in-out hover:bg-white/60">
+              <div className="flex items-center bg-neutral-950 py-1 px-4 rounded-xl transition duration-300">
                 <i className="fas fa-tags mr-2 text-white fa-lg"></i>
                 <p className="text-white font-bold text-sm uppercase">Store</p>
               </div>
+            </div>
             </a>
             {/* <a href="/#">
               <div className="flex items-center bg-neutral-950 py-2 px-5 rounded-xl transition duration-300">
