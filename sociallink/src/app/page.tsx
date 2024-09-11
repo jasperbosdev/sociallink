ffdb1b7470691ac5be45afb69aa4776391cde82c
Lionz <Jasperbos1234@gmail.com>
2024-09-11 21:09:19 +1200

    add animations and additions to page.tsx

--- sociallink/src/app/page.tsx
+++ sociallink/src/app/page.tsx
@@ -11,24 +11,55 @@ export default function Home() {
           <p className="text-6xl font-bold bg-gradient-to-r from-zinc-200/60 via-zinc-200 to-zinc-200/60 
           bg-clip-text text-transparent animate-fade animate-once animate-duration-[2800ms] animate-delay-[600ms]"
           >Komako &gt;w&lt;</p>
-          <p className="mt-1 font-bold text-white/60 animate-fade-up animate-once animate-duration-[1800ms] animate-delay-[900ms]"
+          <p className="mt-1 font-bold text-white/60 animate-fade-up animate-once animate-duration-[1800ms] animate-delay-[600ms]"
           >Build your profile, link your socials, and share your world in one place!</p>
         </div>
 
-        <div className="relative flex justify-center py-10">
+        <div className="relative flex justify-center w-2/3">
 
-          <svg height="90%" width="60%" className="absolute left-0 top-[50%] z-10 translate-y-[-50%] rotate-6 text-indigo-600/50 opacity-100 blur-[60px] animate-fadeInSlow">
+          <svg height="90%" width="60%" className="absolute left-0 top-[50%] z-10 translate-y-[-50%] rotate-6 text-indigo-600/50 opacity-100 blur-[60px]
+          animate-fade animate-once animate-duration-[2800ms] animate-delay-[600ms]">
             <ellipse cx="50%" cy="50%" ry="50%" rx="50%" fill="currentColor"></ellipse>
           </svg>
           
-          <svg height="90%" width="70%" className="absolute right-0 top-[50%] z-10 translate-y-[-50%] text-[hsla(0_80%_51%_/_20%)] opacity-100 blur-[60px] animate-fadeInSlow">
+          <svg height="90%" width="70%" className="absolute right-0 top-[50%] z-10 translate-y-[-50%] text-[hsla(0_80%_51%_/_20%)] opacity-100 blur-[60px]
+          animate-fade animate-once animate-duration-[2800ms] animate-delay-[600ms]">
             <ellipse cx="50%" cy="50%" ry="50%" rx="50%" fill="currentColor"></ellipse>
           </svg>
 
           {/* Image with Tilt effect */}
-          <Tilt className="w-2/6 z-20" lineGlareEnable={false} tiltReverse={true}>
-            <img src="./static/images/placeholdera.png" className="w-full" />
+          <Tilt className="w-2/6 z-20" lineGlareEnable={false} tiltReverse={true} spotGlareEnable={false}>
+            <img src="./static/images/placeholderc.png" className="w-full animate-fade animate-once animate-duration-[2800ms] animate-delay-[600ms]" />
           </Tilt>
+        </div>
+
+        <div className="flex flex-col animate-fade-up animate-once animate-duration-[1500ms] animate-delay-[2100ms]">
+          <p className="font-bold text-white/60">
+            Bla bla bla, blu blu blu, bleh bleh bleh...
+          </p>
+          <div className="flex justify-center space-x-10 mt-5">
+            <a href="/#">
+              <div className="flex items-center bg-neutral-950 py-2 px-5 rounded-xl transition duration-300">
+                <i className="fas fa-tags mr-2 text-white fa-lg"></i>
+                <p className="text-white font-bold text-sm uppercase">Store</p>
+              </div>
+            </a>
+            {/* <a href="/#">
+              <div className="flex items-center bg-neutral-950 py-2 px-5 rounded-xl transition duration-300">
+                <i className="fas fa-store mr-2 text-white fa-lg"></i>
+                <p className="text-white font-bold text-sm uppercase">Store</p>
+              </div>
+            </a> */}
+          </div>
+        </div>
+        
+        <div className="flex flex-col text-center space-y-6 animate-fade-up animate-once animate-duration-[1500ms] animate-delay-[2900ms]">
+          <p className="font-bold text-white/60">
+            Learn More
+          </p>
+          <div className="animate-bounce">
+            <i className='fas fa-arrow-down fa-2xl text-white/60'></i>
+          </div>
         </div>
 
       </div>
