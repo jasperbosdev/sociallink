c398c0a59e844178774bce2a7e5e981fdfaf3a39
Lionz <Jasperbos1234@gmail.com>
2024-09-20 00:25:30 +1200

    make footer like design, wip

--- sociallink/src/app/components/footer.tsx
+++ sociallink/src/app/components/footer.tsx
@@ -1,6 +1,40 @@
+// export default function Footer() {
+//     return (
+//         <>
+//             <div className="flex justify-center py-4 font-bold text-white/80">
+//                 <p className="bg-gradient-to-r from-zinc-200/60 via-zinc-200 to-zinc-200/60 bg-clip-text text-transparent">
+//                     @leeuwz <span className="text-white">🐈</span> &copy; 2024
+//                 </p>
+//             </div>
+//         </>
+//     );
+// }
+
 export default function Footer() {
     return (
         <>
+            <div className="max-w-[1000px] mx-auto flex justify-between border-b-4 border-white/60">
+                {/* logo with socials */}
+                <div className="flex flex-col">
+                    <div className="">
+                        <img src="/static/logo.png" className="w-32 h-auto"></img>
+                    </div>
+                    <div className="flex flex-row space-x-2 mt-2">
+                        <p className="font-bold text-white/60">X</p>
+                        <p className="font-bold text-white/60">X</p>
+                        <p className="font-bold text-white/60">X</p>
+                    </div>
+                </div>
+                {/* help/info section */}
+                <div className="flex flex-row space-x-12">
+                    <div className="flex flex-col">
+                        <p className="font-bold text-white/60">Contact</p>
+                    </div>
+                    <div className="flex flex-col">
+                    <p className="font-bold text-white/60">Contact</p>
+                    </div>
+                </div>
+            </div>
             <div className="flex justify-center py-4 font-bold text-white/80">
                 <p className="bg-gradient-to-r from-zinc-200/60 via-zinc-200 to-zinc-200/60 bg-clip-text text-transparent">
                     @leeuwz <span className="text-white">🐈</span> &copy; 2024
