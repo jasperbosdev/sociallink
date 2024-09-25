eafcfbbe73fe60376626c885db8bceb0db274cd4
Lionz <Jasperbos1234@gmail.com>
2024-09-25 20:01:24 +1200

    Finish footer design

--- sociallink/src/app/components/footer.tsx
+++ sociallink/src/app/components/footer.tsx
@@ -19,19 +19,25 @@ export default function Footer() {
                     <div className="">
                         <img src="/static/logo.png" className="w-32 h-auto"></img>
                     </div>
-                    <div className="flex flex-row space-x-2 mt-2">
-                        <p className="font-bold text-white/60">X</p>
-                        <p className="font-bold text-white/60">X</p>
-                        <p className="font-bold text-white/60">X</p>
+                    <div className="flex flex-row space-x-2 mt-3">
+                    <a href="#/" className="hover:translate-y-[-5px] transition duration-150 hover:text-white/60"><i className="fab fa-x-twitter fa-lg"></i></a>
+                    <a href="https://github.com/leeuwz" target="_blank" className="hover:translate-y-[-5px] transition duration-150 hover:text-white/60"><i className="fab fa-github fa-lg"></i></a>
+                    <a href="#/" className="hover:translate-y-[-5px] transition duration-150 hover:text-white/60"><i className="fab fa-discord fa-lg"></i></a>
                     </div>
                 </div>
                 {/* help/info section */}
                 <div className="flex flex-row space-x-12">
                     <div className="flex flex-col">
-                        <p className="font-bold text-white/60">Contact</p>
+                        <p className="font-bold text-white/60 border-b-4 border-white/60">Contact</p>
+                        <p className="font-bold text-white/60 mt-2">Abuse</p>
+                        <p className="font-bold text-white/60">Privacy</p>
+                        <p className="font-bold text-white/60 mb-3">Support</p>
                     </div>
                     <div className="flex flex-col">
-                    <p className="font-bold text-white/60">Contact</p>
+                        <p className="font-bold text-white/60 border-b-4 border-white/60">Contact</p>
+                        <p className="font-bold text-white/60 mt-2">FAQ</p>
+                        <p className="font-bold text-white/60">Terms of Service</p>
+                        <p className="font-bold text-white/60 mb-3">Privacy Policy</p>
                     </div>
                 </div>
             </div>
