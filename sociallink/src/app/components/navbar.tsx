e25aeb08d5cb6b325a905d5f2fcd23b8a5a9d260
leeuwz <jasperbos1234@gmail.com>
2024-10-04 18:42:03 +0200

    wrap layout nav in a div with assigned id

--- sociallink/src/app/components/navbar.tsx
+++ sociallink/src/app/components/navbar.tsx
@@ -14,36 +14,38 @@ export default function Navbar() {
   const { session } = useSupabase();
 
   return (
-    <nav className="border-b-2 border-zinc-800 sticky top-0 backdrop-blur-lg bg-opacity-70 z-50 animate-fade-down animate-once animate-duration-1000 animate-delay-0">
-      <div className="flex mx-auto justify-between max-w-[1000px] py-4 items-center">
-        <div className="">
-          <a href="/"><img src="/static/logo.png" className='w-32 h-auto'></img></a>
-        </div>
-        <div className="flex font-bold space-x-8 text-l text-zinc-300 items-center">
-          <div className={isActive("/") ? "text-white/60" : "hover:text-white transition duration-300"}>
-            <a className="hover:text-white transition duration-300" href="/#">Home</a>
+    <div id='nav'>
+      <nav className="border-b-2 border-zinc-800 sticky top-0 backdrop-blur-lg bg-opacity-70 z-50 animate-fade-down animate-once animate-duration-1000 animate-delay-0">
+        <div className="flex mx-auto justify-between max-w-[1000px] py-4 items-center">
+          <div className="">
+            <a href="/"><img src="/static/logo.png" className='w-32 h-auto'></img></a>
           </div>
-          <div className=""><a className="hover:text-white transition duration-300" href="/#">Store</a></div>
-          <div className=""><a className="hover:text-white transition duration-300" href="/#">Discord</a></div>
-          <SpotlightEffect>
-            {session ? (
-              <a href="/dashboard">
-                <div className="flex items-center bg-white py-1 px-2 rounded-xl transition duration-300">
-                  <i className="fas fa-address-card mr-2 text-black fa-lg"></i>
-                  <p className="text-black font-extrabold text-base">Dashboard</p>
-                </div>
-              </a>
-            ) : (
-              <a href="/login">
-                <div className="flex items-center bg-white py-1 px-2 rounded-xl transition duration-300">
-                  <i className="fas fa-right-to-bracket mr-2 text-black fa-lg"></i>
-                  <p className="text-black font-extrabold text-base">Log in</p>
-                </div>
-              </a>
-            )}
-          </SpotlightEffect>
+          <div className="flex font-bold space-x-8 text-l text-zinc-300 items-center">
+            <div className={isActive("/") ? "text-white/60" : "hover:text-white transition duration-300"}>
+              <a className="hover:text-white transition duration-300" href="/#">Home</a>
+            </div>
+            <div className=""><a className="hover:text-white transition duration-300" href="/#">Store</a></div>
+            <div className=""><a className="hover:text-white transition duration-300" href="/#">Discord</a></div>
+            <SpotlightEffect>
+              {session ? (
+                <a href="/dashboard">
+                  <div className="flex items-center bg-white py-1 px-2 rounded-xl transition duration-300">
+                    <i className="fas fa-address-card mr-2 text-black fa-lg"></i>
+                    <p className="text-black font-extrabold text-base">Dashboard</p>
+                  </div>
+                </a>
+              ) : (
+                <a href="/login">
+                  <div className="flex items-center bg-white py-1 px-2 rounded-xl transition duration-300">
+                    <i className="fas fa-right-to-bracket mr-2 text-black fa-lg"></i>
+                    <p className="text-black font-extrabold text-base">Log in</p>
+                  </div>
+                </a>
+              )}
+            </SpotlightEffect>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
