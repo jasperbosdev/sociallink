b7057fcd599d126645965da282be7b0385d7105f
Lionz <Jasperbos1234@gmail.com>
2024-10-08 23:20:48 +1300

    move constants to seperate file to save space (/profiles)

---
+++ sociallink/src/app/u/[username]/util/configConsts.ts
@@ -0,0 +1,32 @@
+// /util/configConsts.ts
+export const configConsts = (config: any) => {
+    const borderWidth = (config?.border_width !== undefined && config.border_width !== null) 
+      ? `${config.border_width}px` 
+      : '3px';
+  
+    const borderRadius = (config?.border_radius !== undefined && config.border_radius !== null) 
+      ? `${config.border_radius}rem` 
+      : '0.5rem';
+  
+    const cardOpacity = (config?.card_opacity !== undefined && config.card_opacity !== null)
+      ? config.card_opacity / 100 
+      : 0.9;
+  
+    const cardBlur = (config?.card_blur !== undefined && config.card_blur !== null)
+      ? `backdrop-blur-[${config.card_blur}px]`
+      : 'backdrop-blur-[0px]';
+  
+    const bgBlurValue = (config?.background_blur !== undefined && config.background_blur !== null)
+      ? `${config.background_blur}px`
+      : '0px';
+  
+    const bgBrightnessValue = (config?.background_brightness !== undefined && config.background_brightness !== null)
+      ? config.background_brightness / 100 
+      : 1;
+
+    const usernameFx = (config?.username_fx !== undefined && config.username_fx !== null)
+    ? config.username_fx
+    : false;
+  
+    return { borderWidth, borderRadius, cardOpacity, cardBlur, bgBlurValue, bgBrightnessValue, usernameFx };
+};  
