0b8abb35db3a32a167439137c9f6b38f27d65ff0
leeuwz <jasperbos1234@gmail.com>
2024-11-06 15:48:34 +0100

    finalize discord server display component

--- sociallink/src/app/u/[username]/util/discordServerInfo.tsx
+++ sociallink/src/app/u/[username]/util/discordServerInfo.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import { useFetchDiscordInv } from './fetchDiscordInv';
+import { useUserData } from './userDataLogic';
 import { fetchServerInfo } from './fetchServerInfo';
+import { useFetchConfig } from './fetchConfig';
+import { configConsts } from './configConsts';
 
 interface ServerInfo {
   name: string;
@@ -13,6 +16,8 @@ export default function DiscordServerInfo() {
   const { discordInv, isLoadingDiscordInv } = useFetchDiscordInv();
   const [serverInfo, setServerInfo] = useState<ServerInfo | null>(null);
   const [isLoading, setIsLoading] = useState(true);
+  const { userData, loading: userLoading, error } = useUserData();
+  const { config, loading: configLoading, error: configError } = useFetchConfig(userData?.uid);
 
   useEffect(() => {
     const loadServerInfo = async () => {
@@ -34,14 +39,72 @@ export default function DiscordServerInfo() {
     return <p>Unable to load server information.</p>; // Error or no data
   }
 
+  const {
+    borderWidth,
+    borderRadius,
+    cardOpacity,
+    cardBlur,
+    bgBlurValue,
+    bgBrightnessValue,
+    usernameFx,
+    cardGlow,
+    pfpDecoration,
+    decorationValue,
+    cardTilt,
+    showBadges,
+    fullRoundedSocials,
+    primaryColor,
+    secondaryColor,
+    accentColor,
+    textColor,
+    embedColor,
+    backgroundColor,
+    profileFont,
+    usernameFxColor,
+    useBanner,
+    useAutoplayFix,
+    useBackgroundAudio,
+  } = configConsts(config);
+
   return (
-    <div className='flex'>
-      {serverInfo.iconUrl && (
-        <img src={serverInfo.iconUrl} className='rounded-2xl' alt={`${serverInfo.name} Server Icon`} width="90" height="90" />
-      )}
-      <p><strong>Server Name:</strong> {serverInfo.name}</p>
-      <p><strong>Member Count:</strong> {serverInfo.memberCount}</p>
-      <p><strong>Online Count:</strong> {serverInfo.onlineCount}</p>
+    <div className='flex justify-between w-full'>
+      <div className="flex">
+        {serverInfo.iconUrl && (
+          <img 
+            src={serverInfo.iconUrl} 
+            className='rounded-2xl mr-2' 
+            alt={`${serverInfo.name} Server Icon`} 
+            style={{ width: '80px', height: 'auto' }} 
+          />
+        )}
+        <div className="flex-col text-base items-center">
+          <p className='font-black text-lg'>/{serverInfo.name}</p>
+          <div className='flex items-center'>
+            <svg height="10" width="10" className='mr-1'>
+              <circle cx="5" cy="5" r="5" fill="#32a852" />
+            </svg>
+            <p className='text-base'>{serverInfo.onlineCount} Online</p>
+          </div>
+          <div className='flex items-center'>
+            <svg height="10" width="10" className='mr-1'>
+              <circle cx="5" cy="5" r="5" fill="#808080" />
+            </svg>
+            <p className='text-base'>{serverInfo.memberCount} Members</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center cursor-pointer"
+      onClick={() => window.open(discordInv[0].discord_link, '_blank')}>
+        <div className='flex discord-join transition-all border border-[2px] p-2 items-center'
+        style={{
+          borderColor: `rgb(${accentColor})`,
+          borderRadius,
+          backgroundColor: `rgba(${primaryColor}, ${cardOpacity})`,
+        }}>
+          <p>Join Server</p>
+          <i className="fas fa-arrow-right ml-2 text-lg join-arrow transition-transform duration-300 transform"></i>
+        </div>
+      </div>
     </div>
   );
-}
+};  
