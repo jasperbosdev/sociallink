624ee65796fe0f88e140434fb82059875a97ed89
Lionz <Jasperbos1234@gmail.com>
2024-11-30 01:09:17 +1300

    make footer responsive

--- sociallink/src/app/components/footer.tsx
+++ sociallink/src/app/components/footer.tsx
@@ -14,20 +14,20 @@ export default function Footer() {
     return (
         <>
             <div id="footer">
-                <div className="max-w-[1000px] mx-auto flex justify-between border-b-4 border-white/60">
+                <div className="px-8 max-w-[1000px] mx-auto flex flex-col md:flex-row justify-between border-b-4 border-white/60">
                     {/* logo with socials */}
                     <div className="flex flex-col">
-                        <div className="">
+                        <div className="flex justify-center md:justify-start">
                             <img src="/static/logo.png" className="w-32 h-auto"></img>
                         </div>
-                        <div className="flex flex-row space-x-2 mt-3">
+                        <div className="flex flex-row space-x-2 mt-3 justify-center md:justify-start">
                         <a href="#/" className="hover:translate-y-[-5px] transition duration-150 hover:text-white/60"><i className="fab fa-x-twitter fa-lg"></i></a>
                         <a href="https://github.com/leeuwz" target="_blank" className="hover:translate-y-[-5px] transition duration-150 hover:text-white/60"><i className="fab fa-github fa-lg"></i></a>
                         <a href="#/" className="hover:translate-y-[-5px] transition duration-150 hover:text-white/60"><i className="fab fa-discord fa-lg"></i></a>
                         </div>
                     </div>
                     {/* help/info section */}
-                    <div className="flex flex-row space-x-12">
+                    <div className="py-4 md:py-0 flex flex-row space-x-8 md:space-x-12 justify-center md:justify-start text-center md:text-start">
                         <div className="flex flex-col">
                             <p className="font-bold text-white/60 border-b-4 border-white/60">Contact</p>
                             <p className="font-bold text-white/60 mt-2">Abuse</p>
