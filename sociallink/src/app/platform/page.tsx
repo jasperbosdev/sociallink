5871adf34bbe84a43682afa6c1988f10ed5a666b
leeuwz <jasperbos1234@gmail.com>
2024-12-06 00:17:08 +0100

    add Platform Guidelines page

---
+++ sociallink/src/app/platform/page.tsx
@@ -0,0 +1,92 @@
+export default function Platform() {
+    return (
+        <>
+            <div className="px-4">
+                <div className="max-w-[1024px] mx-auto w-full bg-[#101013] border-4 border-white/20 rounded-xl my-12">
+                    <h1 className="font-black text-center text-4xl py-8">
+                        Komako's Platform Guidelines
+                    </h1>
+                    {/* start */}
+                    <div className="px-4 sm:px-8">
+                        {/* introduction */}
+                        <div className="py-2">
+                            <div className="py-2 font-semibold">
+                                <p>
+                                    These guidelines are in place to ensure Komako remains an abuse-free platform.
+                                    Violating these guidelines can result in a ban. If you become aware of anyone
+                                    breaking these rules, please report them to us through our Discord server or by
+                                    <a href="mailto:dxmpup@proton.me?subject=Abuse%20Report%20Komako's%20sociallink" className="text-indigo-400"> dxmpup@proton.me</a>.
+                                </p>
+                            </div>
+                        </div>
+                        {/* Rules */}
+                        <div className="py-2">
+                            <div className="py-2 font-semibold">
+                                <h2 className="text-[1.3rem] text-indigo-400 font-bold">Do not ban evade</h2>
+                                <p>
+                                    Once banned, you are not allowed to create a new account. Any new accounts created after being banned will also be banned.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="py-2">
+                            <div className="py-2 font-semibold">
+                                <h2 className="text-[1.3rem] text-indigo-400 font-bold">Do not share your account credentials</h2>
+                                <p>
+                                    Komako accounts are for individual use only. Sharing your account or access credentials may lead to a ban, depending on the situation.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="py-2">
+                            <div className="py-2 font-semibold">
+                                <h2 className="text-[1.3rem] text-indigo-400 font-bold">Do not upload real-life media depicting gore, excessive violence, animal abuse, or self-harm</h2>
+                                <p>
+                                    Uploading content that contains any of these topics will result in an immediate ban.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="py-2">
+                            <div className="py-2 font-semibold">
+                                <h2 className="text-[1.3rem] text-indigo-400 font-bold">Do not upload content depicting or promoting child sexual abuse material (CSAM)</h2>
+                                <p>
+                                    Uploading content related to CSAM will result in an immediate ban, and we will report it to the authorities.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="py-2">
+                            <div className="py-2 font-semibold">
+                                <h2 className="text-[1.3rem] text-indigo-400 font-bold">Do not use our tools for spam, phishing, spreading malware, screamer links, or scamming</h2>
+                                <p>
+                                    Using our platform for these activities will almost always result in a ban.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="py-2">
+                            <div className="py-2 font-semibold">
+                                <h2 className="text-[1.3rem] text-indigo-400 font-bold">Do not spam our tools and API</h2>
+                                <p>
+                                    Spamming uploads, or any other functions on our website will result in a ban.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="py-2">
+                            <div className="py-2 font-semibold">
+                                <h2 className="text-[1.3rem] text-indigo-400 font-bold">Do not abuse exploits or bugs</h2>
+                                <p>
+                                    Abusing any known exploit or bug instead of reporting it will lead to a ban.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="py-2">
+                            <div className="py-2 font-semibold">
+                                <h2 className="text-[1.3rem] text-indigo-400 font-bold">Do not sell or trade accounts or invites</h2>
+                                <p>
+                                    Selling or trading accounts or invites is prohibited and will result in a ban.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
